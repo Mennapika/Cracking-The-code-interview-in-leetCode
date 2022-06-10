13ecad986907f8195aec309f809208156b9f848b
Menna Naiem <38566183+Mennapika@users.noreply.github.com>
2022-06-10 18:32:44 +0200

    Solving Problem #217. Contains Duplicate
    Similar to probelm 1.1 IsUnique in the book

---
+++ Ch1.Array/Is Duublicated.java
@@ -0,0 +1,43 @@
+//Hash Map Solution
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       Map<Integer,Integer> map = new HashMap<>();
+        for(int i : nums){
+            if(map.containsKey(i))return true;
+            map.put(i,1);
+        }
+        return false;
+    }
+}
+
+//Set Solution 
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+    if(nums==null || nums.length==0)
+        return false;
+ 
+    HashSet<Integer> set = new HashSet<Integer>();
+    for(int i: nums){
+        if(!set.add(i)){
+            return true;
+        }
+    }
+ 
+    return false;
+}
+    }
+//ArrayList  Time limited Exceed 
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       ArrayList<Integer> List= new ArrayList<>();
+        if(nums==null || nums.length==0){
+            return false;
+        }
+        for(int i : nums){
+            if(List.contains(i))return true;
+            List.add(i);
+        }
+        return false;
+    }
+}
